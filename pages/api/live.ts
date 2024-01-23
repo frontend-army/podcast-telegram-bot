c4e0565db9baa9e0b67bbb66ca98a80da594bdb2
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-01-23 17:21:42 +0100

    Changed tweet message

--- pages/api/live.ts
+++ pages/api/live.ts
@@ -3,9 +3,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { tweetMessage } from '../../services/twitter';
 
 export default async function handler(request: NextRequest, response: NextResponse) {
-  console.log(request.body);
-  return response.status(200).json({ published: 1 });
-  return await tweetMessage(`🔴ESTAMOS LIVE!!!🔴
+  await tweetMessage(`🔴ESTAMOS LIVE!!!🔴
+${request.body!.title}
 Los esperamos! 👋 🔥🔥🔥
 https://www.twitch.tv/frontend_army`);
+return response.status(200).send('ok');
 }
