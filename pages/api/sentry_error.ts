d571e3fa13cac33ebc025338cd850d9356842057
FrankIglesias <ifrancisco.iglesias@gmail.com>
2024-02-06 12:16:55 +0100

    Modified endpoint

--- pages/api/sentry_error.ts
+++ pages/api/sentry_error.ts
@@ -2,7 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { sendMessage } from '../../services/telegram';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  console.log(request.body);
-  const res = await sendMessage(request.body);
-  return response.status(200).json({ message: `${res.data.length} Tweets sent`});
+    const {action, data} = request.body;
+  
+    if (data.issue && action === "created") {
+      await sendMessage(process.env.CHAT_ID, `New issue created: ${data.issue.title}, link: https://frontend-army.sentry.io/issues/${data.issue.id}`);
+    }
+
+  return response.status(200).json({ message: 'Message sent'});
 }
