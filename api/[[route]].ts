f32ccf649bbea9f850f3210b1c0aeeb98032c275
Francisco <francisco@Franciscos-MacBook-Pro.local>
2026-07-06 20:57:16 +0200

    remove unused HTTP method exports from api/[[route]].ts

--- api/[[route]].ts
+++ api/[[route]].ts
@@ -3,7 +3,3 @@ import app from "../src/index";
 
 export const GET = handle(app);
 export const POST = handle(app);
-export const PUT = handle(app);
-export const PATCH = handle(app);
-export const DELETE = handle(app);
-export const OPTIONS = handle(app);
